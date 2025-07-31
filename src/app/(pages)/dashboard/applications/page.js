0d91ccd13f1958d2826f50ea3658af120d08b43d
Ajaz Miah <ajazmiah@yahoo.com>
2025-07-31 02:56:25 -0400

    dal

--- src/app/(pages)/dashboard/applications/page.js
+++ src/app/(pages)/dashboard/applications/page.js
@@ -4,7 +4,7 @@ import { useState, useEffect, useMemo } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import getUserApplications from '../../../DataAccessLayer/getUserApplications'
+import getUserApplications from '../../../lib/DataAccessLayer/server/getUserApplications'
 
 import {
   Select,
