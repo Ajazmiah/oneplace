f476bc0bb917cc7528d8cd0fe647eb7141a47d25
Ajaz Miah <ajazmiah@yahoo.com>
2025-08-11 02:33:58 -0400

    viewing specific job

--- src/app/lib/DataAccessLayer/applications.js
+++ src/app/lib/DataAccessLayer/applications.js
@@ -2,6 +2,7 @@ import AddApplicationModel from "@/database/models/addApplicationModel";
 import { getUserByEmail } from "@/app/lib/utils/databaseUtils";
 import { getUserSession } from "./getSession";
 
+
 export const getApplications = async () =>  {
   const session = await getUserSession();
   const user = await getUserByEmail(session.user.email);
@@ -9,11 +10,27 @@ export const getApplications = async () =>  {
   const applications = await AddApplicationModel.find({
     userId: user._id,
   }).populate({ path: "userId" });
+  console.log("APPLI", applications)
 
   return applications;
 }
 
-export const getSingleApplication = async(id) => {}
+export const getSingleApplication = async ( id) => {
+    try {
+      await getUserSession();
+  
+      const application = await AddApplicationModel.findById({_id:id});
+  
+      if (!application) {
+        throw new Error("Application not found");
+      }
+  
+      return application;
+    } catch (error) {
+      console.error("Error fetching application:", error.message);
+      throw error; // rethrow so the caller can handle it
+    }
+  };
 
 export const editApplication = async(id) => {}
 
