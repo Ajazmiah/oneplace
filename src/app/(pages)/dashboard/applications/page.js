4b2a938e3c778fc0aa43c1b57626cbdb248148b7
Ajaz Miah <ajazmiah@yahoo.com>
2025-08-10 21:54:26 -0400

    idk

--- src/app/(pages)/dashboard/applications/page.js
+++ src/app/(pages)/dashboard/applications/page.js
@@ -1,9 +1,8 @@
+import { getApplications } from "@/app/lib/DataAccessLayer/applications";
 import ApplicationTable from "@/components/Applications/ApplicationTable";
-import getApplications from "@/app/lib/DataAccessLayer/getUserApplications";
 
 export default async function JobApplicationsPage() {
   const applications = JSON.parse(JSON.stringify(await getApplications()));
-  
 
   return <ApplicationTable applications={applications} />;
 }
