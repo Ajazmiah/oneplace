9777a0716ab56d4d734b3f63c90ab28e5968bf18
Ajaz Miah <ajazmiah@yahoo.com>
2026-09-03 18:17:35 -0400

    perf(web-app): memoize getUserByEmail per request
    - Wrap getUserByEmail in React cache() so repeated lookups for the same email within one request reuse the result instead of re-querying Mongo

--- apps/onetrack-web-app/src/app/lib/utils/databaseUtils.js
+++ apps/onetrack-web-app/src/app/lib/utils/databaseUtils.js
@@ -1,7 +1,8 @@
+import { cache } from "react";
 import { connectDb } from "@/database/dbConnection";
 import userModel from "@/database/models/userModel";
-export const getUserByEmail = async (email) => {
-  await connectDb();
 
+export const getUserByEmail = cache(async (email) => {
+  await connectDb();
   return await userModel.findOne({ email });
-};
+});
